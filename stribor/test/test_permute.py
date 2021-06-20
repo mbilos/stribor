12f6be700ff2df4468f4e3bf5c0c2d21e4d03bf5
Marin Bilos <mbbilos@amazon.com>
2021-06-20 12:14:06 +0200

    Add permute transformation

--- stribor/test/test_permute.py
+++ stribor/test/test_permute.py
@@ -5,15 +5,26 @@ import stribor as st
 from stribor.test.base import check_inverse, check_jacobian
 
 @pytest.mark.parametrize('input_shape', [(1, 1), (2, 10), (10, 2), (7, 4, 5)])
-def test_reverse(input_shape):
-    np.random.seed(123)
+@pytest.mark.parametrize('dims', [[-1], [0], [0, 1]])
+def test_flip(input_shape, dims):
     torch.manual_seed(123)
 
-    model = st.Reverse()
+    model = st.Flip(dims)
     x = torch.randn(*input_shape)
 
     y, log_jac_y = model.forward(x)
     x_, log_jac_x = model.inverse(y)
 
     check_inverse(x, x_)
-    check_jacobian(log_jac_x, log_jac_y)
+
+@pytest.mark.parametrize('input_shape', [(1, 1), (2, 10), (10, 2), (7, 4, 5)])
+def test_permute(input_shape):
+    torch.manual_seed(123)
+
+    model = st.Permute(input_shape[-1])
+    x = torch.randn(*input_shape)
+
+    y, log_jac_y = model.forward(x)
+    x_, log_jac_x = model.inverse(y)
+
+    check_inverse(x, x_)
