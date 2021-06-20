1c834516a095d83064b3f0559ea3b76322629478
Marin Bilos <mbbilos@amazon.com>
2021-06-20 12:14:27 +0200

    Clean sigmoid transformation

--- stribor/flows/sigmoid.py
+++ stribor/flows/sigmoid.py
@@ -1,32 +1,36 @@
-# https://github.com/pytorch/pytorch/blob/master/torch/distributions/transforms.py#L349
-
 import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+# Code adapted from torch.distributions.transforms
+
 
 class Sigmoid(nn.Module):
-    """ Sigmoid flow. """
+    """
+    Sigmoid transformation.
+    """
     def __init__(self, **kwargs):
         super().__init__()
 
     def forward(self, x, **kwargs):
         finfo = torch.finfo(x.dtype)
         y = torch.clamp(torch.sigmoid(x), min=finfo.tiny, max=1. - finfo.eps)
-        log_jac = -F.softplus(-x) - F.softplus(x)
-        return y, log_jac
+        ljd = -F.softplus(-x) - F.softplus(x)
+        return y, ljd
 
     def inverse(self, y, **kwargs):
         finfo = torch.finfo(y.dtype)
         y = y.clamp(min=finfo.tiny, max=1.0 - finfo.eps)
 
         x = y.log() - (-y).log1p()
-        log_jac = -y.log() - (-y).log1p()
-        return x, log_jac
+        ljd = -y.log() - (-y).log1p()
+        return x, ljd
 
 class Logit(nn.Module):
-    """ Logit flow. Inverse of sigmoid function. """
+    """
+    Logit transformation. Inverse of sigmoid.
+    """
     def __init__(self, **kwargs):
         super().__init__()
         self.base_flow = Sigmoid(**kwargs)
