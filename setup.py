5f42acdf4fb088891aee89cc151290df019ba891
Marin Bilos <mbbilos@amazon.com>
2021-06-23 11:13:50 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,4 +4,8 @@ setup(name='stribor',
       version='0.1.0',
       description='normalizing flows and neural flows',
       packages=find_packages('.'),
-      zip_safe=False)
+      packages=setuptools.find_packages(),
+      install_requires=['numpy>=1.20.1', 'torch>=1.8.0', 'torchdiffeq==0.2.1']
+      python_requires='>=3.6',
+      zip_safe=False
+)
